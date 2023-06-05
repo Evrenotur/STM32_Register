2b792aabd9efada37b4458dc8f8f03dd0652c0b1
Evrenotur <101205887+Evrenotur@users.noreply.github.com>
2023-06-05 20:10:01 +0300

    Update microfon.ino

--- microfon_arduino/microfon/microfon.ino
+++ microfon_arduino/microfon/microfon.ino
@@ -1,124 +1,129 @@
-/*
-  modified on Sep 8, 2020
-  Modified by MohammedDamirchi from Arduino Examples
-  Home
-*/
+//Kullanıcıdan veri alalım
+#include<Servo.h>
 
-#define led1   3
-#define led2   4
-#define led3   5
-#define led4   6
-#define button 7
+char veri;
+int mesafe, tahmin, pot_deger, ldr_deger;
 
-int sen1,sen2,sen3,sen4,art;
-int value1,value2,value3,value4;
+const int trigPin = 9;    //Ultrasonic sensörün trig pini
+const int echoPin = 10;   //Ultrasonic sensörün echo pini
+const int redPin = 2;     //Kırmızı led pin
+const int greenPin = 3;   //Yeşil led pin
+const int bluePin = 4;    //Mavi led pin
+const int yellowPin = 5;  //Sarı led pin
+const int buzzerPin = 6;  //Buzzer pin
+const int servoPin = 7;   //Servo motor pin
+const int potPin = A0;    //Potansiyometre pin
+const int ldrPin = A1;    //LDR pin
 
-
+Servo myservo;
 
 void setup() {
- 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  myservo.attach(servoPin);
   Serial.begin(9600);
-
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
-  pinMode(led4,OUTPUT);
-  pinMode(button,INPUT);
 }
 
-
 void loop() {
-if (!button) {
-      delay(50); 
-       art++;
-      while(!button){ 
+  //Kullanıcıdan veri alalım
+  Serial.println("a, b, c veya d girin:");
+  while (!Serial.available());
+  veri = Serial.read();
+
+  if (veri == 'a') {
+    //Mesafe ölçümü yapalım
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    mesafe = pulseIn(echoPin, HIGH) / 58.2;
+
+    //Mesafe değerini ekrana yazdıralım
+    Serial.print("Mesafe: ");
+    Serial.print(mesafe);
+    Serial.println(" cm");
+
+    //Mesafe değerine göre işlemler yapalım
+    if (mesafe >= 15 && mesafe <= 25) {
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(bluePin, HIGH);
+        myservo.write(0);
+        tone(buzzerPin, 1000);
+        delay(300);
+        digitalWrite(bluePin, LOW);
+        myservo.write(90);
+        noTone(buzzerPin);
+        delay(300);
       }
-      delay(10); 
-      } 
-      
- if(art==1)
- {
-    digitalWrite(led1,HIGH);
-    delay(500);
-    digitalWrite(led1,LOW);
-    delay(500);
-  value1=analogRead(A4); 
-  Serial.println(value1);
- }
- else if(art==2)
- {
-   digitalWrite(led2,HIGH);
-    delay(500);
-    digitalWrite(led2,LOW);
-    delay(500);
-   value2=analogRead(A4);
-     Serial.println(value2);
- }
- else if(art==3)
- {
-   digitalWrite(led3,HIGH);
-    delay(500);
-    digitalWrite(led3,LOW);
-    delay(500);
-    value3=analogRead(A4);
-      Serial.println(value3);
- }
- else if(art==4)
- {
-   digitalWrite(led4,HIGH);
-    delay(500);
-    digitalWrite(led4,LOW);
-    delay(500);
-   value4=analogRead(A4);
-     Serial.println(value4);
- }
- 
-  else 
- {
-   art=0;
- }
+    } else if (mesafe >= 4 && mesafe <= 10) {
+      for (int i = 0; i < 10; i++) {
+        digitalWrite(redPin, HIGH);
+        myservo.write(90);
+        tone(buzzerPin, 2000);
+        delay(100);
+        digitalWrite(redPin, LOW);
+        myservo.write(0);
+        noTone(buzzerPin);
+        delay(100);
+      }
+    } else {
+      Serial.println("Mesafe aralığında değil.");
+    }
+  } else if (veri == 'b') {
+    //Rastgele sayı üretelim
+    tahmin = random(0,6);
 
-
- sen1 = analogRead(A0);
-    sen2 = analogRead(A1);
-    sen3 = analogRead(A2);
-    sen4 = analogRead(A3);
-
-   if(value1<sen1)
-   {
-    digitalWrite(led1,HIGH);
-   }
-   else
-   {
-    digitalWrite(led1,LOW);
-   }
-   if(value2<sen2)
-   {
-    digitalWrite(led2,HIGH);
-   }
-   else
-   {
-    digitalWrite(led2,LOW);
-   }
-    if(value3<sen3)
-   {
-    digitalWrite(led3,HIGH);
-   }
-   else
-   {
-    digitalWrite(led3,LOW);
-   }
- if(value4<sen4)
-   {
-    digitalWrite(led4,HIGH);
-   }
-   else
-   {
-    digitalWrite(led4,LOW);
-   }
-  
- 
-
-      
-
+   if(tahmin==2){
+    digitalWrite(redPin, HIGH);
+    Serial.println("kirmizi led yaniyor");
   }
+else if(tahmin==3){
+    digitalWrite(redPin, HIGH);
+    delay(1000);
+    digitalWrite(yellowPin, HIGH);
+    delay(1000);
+    digitalWrite(greenPin, HIGH);
+    delay(1000);
+    digitalWrite(bluePin, HIGH);
+    
+  }
+  }
+
+  else if(veri=='c'){
+       int pot =analogRead(potPin);
+        Serial.println(pot);
+        delay(pot);
+         digitalWrite(redPin, HIGH);
+         for (int i = 0; i <= 90; i++) {
+    myservo.write(i);
+    delay(15);
+  }
+  delay(pot);
+ digitalWrite(redPin, LOW);
+  delay(pot);
+
+    
+  }
+  else if(veri=='d'){
+   int ldr= analogRead(ldrPin);
+   if(ldr<500){
+     digitalWrite(redPin, HIGH);
+   }
+   else{
+    digitalWrite(redPin, LOW);
+   }
+  }
+ 
+  else{
+    Serial.println("lutfen a,b,c,d degerlerini giriniz");
+  }
+  
+}
+
+  
